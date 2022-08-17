dac362badcceb459602884020e33180b892609b7
Digital <informdigitalmarketing@gmail.com>
2022-08-17 21:47:53 +1000

    API Changes

--- src/getList.js
+++ src/getList.js
@@ -1,6 +1,7 @@
 export async function getList() {
   const data = await fetch(
-    "https://dummy.restapiexample.com/api/v1/employees"
+    "https://mockend.com/InformDGM/MockendDB/users"
+    //"https://dummy.restapiexample.com/api/v1/employees"
   );
   return await data.json();
 }
