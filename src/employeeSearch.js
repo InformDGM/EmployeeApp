d7fcb5ebf1b321f2806d54c59b9962b33bdeb83c
Digital <informdigitalmarketing@gmail.com>
2022-08-27 17:58:12 +1000

    changes

--- src/employeeSearch.js
+++ src/employeeSearch.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { LineAxisOutlined } from "@mui/icons-material";
+import { response } from "express";
 
 export default function EmployeeSearch() {
-  return <div>Search for an Employee</div>;
+  const [userName, setUserName] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://mockend.com/InformDGM/MockendDB/users`)
+      .then((response) => {
+        setUserName(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Search for an Employee </h1>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e = setUserName(e.target.value))}
+      />
+    </div>
+  );
 }
