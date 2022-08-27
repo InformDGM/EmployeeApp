76e0112c75cc69d9e3bfde161376193d21b46657
Digital <informdigitalmarketing@gmail.com>
2022-08-27 18:26:54 +1000

    changes

--- src/employeeSearch.js
+++ src/employeeSearch.js
@@ -4,22 +4,60 @@ import { LineAxisOutlined } from "@mui/icons-material";
 import { response } from "express";
 
 export default function EmployeeSearch() {
-  const [userName, setUserName] = useState([]);
+  const [APIData, setAPIData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   useEffect(() => {
-    axios
-      .get(`https://mockend.com/InformDGM/MockendDB/users`)
-      .then((response) => {
-        setUserName(response.data);
-      });
+    const fetchData = async () => {
+      const result = await axios(
+        "https://mockend.com/InformDGM/MockendDB/users/"
+      );
+      setAPIData(response.data);
+    };
+    fetchData();
   }, []);
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = APIData.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(APIData);
+    }
+  };
   return (
-    <div>
-      <h1>Search for an Employee </h1>
+    <div style={{ padding: 20 }}>
       <Input
         icon="search"
         placeholder="Search..."
-        onChange={(e = setUserName(e.target.value))}
+        onChange={(e) => searchItems(e.target.value)}
       />
+      <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
+        {searchInput.length > 1
+          ? filteredResults.map((item) => {
+              return (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>{item.firstName}</Card.Header>
+                  </Card.Content>
+                </Card>
+              );
+            })
+          : APIData.map((item) => {
+              return (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>{item.firstName}</Card.Header>
+                  </Card.Content>
+                </Card>
+              );
+            })}
+      </Card.Group>
     </div>
   );
 }
